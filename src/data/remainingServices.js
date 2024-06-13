abcdf755426b6d474e473b9bded955986f65d250
GarudaFX <franfra10j@gmail.com>
2024-06-13 20:40:05 +0800

    feat: :recycle: cleaning code by removing repetitive divs and change animations

---
+++ src/data/remainingServices.js
@@ -0,0 +1,14 @@
+const remainingServices = [
+	{
+		title: ["Mobile", "Applications"],
+		description:
+			"We develop cross-platform mobile apps that are compatible with different mobile operating systems",
+	},
+	{
+		title: ["Landing Pages or", "Static Websites"],
+		description:
+			"We design and develop modern websites that are pleasing to the customers' clients.",
+	},
+];
+
+export default remainingServices;
