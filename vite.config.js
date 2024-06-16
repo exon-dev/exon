ac606422b58fede1743db3b8b63deff850153270
GarudaFX <franfra10j@gmail.com>
2024-06-16 14:49:08 +0800

    fix: :bug: bugfix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,19 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"react-type-animation": "react-type-animation/dist/esm/index.es.js",
+		},
+	},
 	build: {
 		rollupOptions: {
 			external: ["react-type-animation"],
+			output: {
+				globals: {
+					"react-type-animation": "TypeAnimation",
+				},
+			},
 		},
 	},
 	optimizeDeps: {
